c6889752025d9c8a6d580d19f2c134ca9d7bc2d2
srijan-shetty <srijanssnl406@gmail.com>
2022-02-02 09:42:45 +0530

    created a global fn to handle result

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,30 +4,34 @@ let currentResult = defaultResult;
 const getUserInput = () => {
   return parseInt(userInput.value);
 };
+const outputfn = (operator, initialResult, enteredNum) => {
+  const currentDesc = `${initialResult} ${operator} ${enteredNum}`;
+  outputResult(currentResult, currentDesc);
+};
 
 const add = () => {
   const enteredValue = getUserInput();
-  const currentDesc = `${currentResult} + ${enteredValue}`;
+  const initialValue = currentResult;
   currentResult = currentResult + enteredValue;
-  outputResult(currentResult, currentDesc);
+  outputfn("+", initialValue, enteredValue);
 };
 const multiply = () => {
   const enteredValue = getUserInput();
-  const currentDesc = `${currentResult} * ${enteredValue}`;
+  const initialValue = currentResult;
   currentResult = currentResult * enteredValue;
-  outputResult(currentResult, currentDesc);
+  outputfn("*", initialValue, enteredValue);
 };
 const substract = () => {
   const enteredValue = getUserInput();
-  const currentDesc = `${currentResult} - ${enteredValue}`;
+  const initialValue = currentResult;
   currentResult = currentResult - enteredValue;
-  outputResult(currentResult, currentDesc);
+  outputfn("-", initialValue, enteredValue);
 };
 const divide = () => {
   const enteredValue = getUserInput();
-  const currentDesc = `${currentResult} / ${enteredValue}`;
+  const initialValue = currentResult;
   currentResult = currentResult / enteredValue;
-  outputResult(currentResult, currentDesc);
+  outputfn("/", initialValue, enteredValue);
 };
 
 addBtn.addEventListener("click", add);
